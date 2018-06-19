9122d9eae535ac3b5ecbd960ecc0c725b7accc47
zelzhan <elzhan.zeinulla@nu.edu.kz>
2018-06-19 17:04:02 +0600

    bitwise difference

---
+++ bitwise-diff.py
@@ -0,0 +1,12 @@
+#Write a function to count the number of bits that are different between two numbers
+
+def bitwise(var1, var2):
+    mask = var1 ^ var2
+    counter = 0
+    while(mask!=0):
+        counter += mask & 1
+        mask = mask >> 1
+    return counter
+
+if __name__ == '__main__':
+    print(count(88, 122))
