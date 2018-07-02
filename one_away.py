6ced6bebb399b805ba120dfd750d56cd034108b1
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-07-02 18:13:47 +0600

    one away implemented

---
+++ one_away.py
@@ -0,0 +1,19 @@
+"""There are three types of edits that can be performed on strings: insert a character,
+remove a character, or replace a character. Given two strings, write a function to check if they are
+one edit (or zero edits) away. """
+
+def one_away(string1, string2):
+	string1 = list(string1)
+	string2 = list(string2)
+	num_edits  = abs(len(string1) - len(string2))
+	while len(string1) != 0:
+		temp = string1.pop()
+		if temp in string2:
+			string2.remove(temp)
+	num_edits += len(string2)
+	return True if num_edits < 2 else False
+
+
+
+if __name__ == '__main__':
+    print(one_away("pale", "bake"))
