c155cadb86425b57219c87b3417a57185e25372f
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-07-02 16:53:02 +0600

    is Unique implemented

---
+++ isUnique.py
@@ -0,0 +1,13 @@
+"""Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures? """
+
+def checker(string):
+    while string != "":
+        temp = string[0]
+        string = string[1:]
+        if temp in string:
+            return True
+    return False
+
+if __name__ == '__main__':
+    print(checker("hesl"))
