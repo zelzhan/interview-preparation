e178d0b1f35bbc6f95a8a3ab3fff1cac6f577db7
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-07-03 17:46:10 +0600

    Update isUnique.py

--- isUnique.py
+++ isUnique.py
@@ -1,6 +1,8 @@
 """Implement an algorithm to determine if a string has all unique characters. What if you
 cannot use additional data structures? """
 
+#time complexity O(N^2)
+
 def checker(string):
     while string != "":
         temp = string[0]
