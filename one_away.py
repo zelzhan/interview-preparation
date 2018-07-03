32d7afab50b8b532b135cc7f3bbe2fdf97316e05
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-07-03 19:32:50 +0600

    Update one_away.py

--- one_away.py
+++ one_away.py
@@ -2,19 +2,30 @@
 remove a character, or replace a character. Given two strings, write a function to check if they are
 one edit (or zero edits) away. """
 
-# time complexity O(N^2)
-def one_away(string1, string2):
-	string1 = list(string1)
-	string2 = list(string2)
-	num_edits  = abs(len(string1) - len(string2))
-	while len(string1) != 0:
-		temp = string1.pop()
-		if temp in string2:
-			string2.remove(temp)
-	num_edits += len(string2)
-	return True if num_edits < 2 else False
+# time complexity O(N)
+def one_away(stringg1, stringg2):
+  string1 = list(stringg1)
+  string2 = list(stringg2)
+  num_edits1  = abs(len(string1) - len(string2))
+  while len(string1) != 0:
+    temp1 = string1.pop()
+    if len(string2) != 0:
+      temp2 = string2.pop()
+      if temp1 != temp2: num_edits1+=1
+  
 
-
+  string1 = list(stringg1)[::-1]
+  string2 = list(stringg2)[::-1]
+  num_edits2  = abs(len(string1) - len(string2))
+  while len(string1) != 0:
+    temp1 = string1.pop()
+    if len(string2) != 0:
+      temp2 = string2.pop()
+      if temp1 != temp2: num_edits2+=1
+  
+  return True if num_edits1 < 2 or num_edits2 < 2 else False
+    
 
 if __name__ == '__main__':
-    print(one_away("pale", "bake"))
+    print(one_away("apale", "pale"))
+
