e3c6c765ec2e1082009e801e6cb6ec963dd579b5
Elzhan <elzhan.zeinulla@nu.edu.kz>
2018-07-10 16:18:07 +0600

    Create delete_middle_node.py

---
+++ delete_middle_node.py
@@ -0,0 +1,23 @@
+"""Implement an algorithm to delete a node in the middle (i.e., any node but
+the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+that node."""
+
+#Time complexity O(N/2)
+def delete_middle(list_):
+  index = int(len(list_)/2)
+  counter = 0
+  head = list_.head
+  while(head.next):
+    counter+=1
+    if counter == index:
+      head.next = head.next.next
+      return
+    head = head.next
+
+
+if __name__ == '__main__':
+  list_ = LinkedList()
+  list_.generate(10, 0, 30)
+  print(list_)
+  delete_middle(list_)
+  print(list_)
